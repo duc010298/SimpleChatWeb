0e42b28b27daaa718220c47652cd49028df8d46b
duc010298 <duc010298@gmail.com>
2019-03-04 13:30:40 +0700

    build project

---
+++ WebChat/WebChat/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebChat.Models
+{
+    public class LoginModel
+    {
+        //Prop for login
+        [Required(ErrorMessage = "Không được để trống tên đăng nhập")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Không được để trống mật khẩu")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool IsRemember { get; set; }
+    }
+}
