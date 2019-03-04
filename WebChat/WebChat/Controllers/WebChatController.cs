75f791e826e6d6fb8d8284db1f1aa04ee12f3e53
NguyenDucThien98 <39368428+NguyenDucThien98@users.noreply.github.com>
2019-03-04 23:47:52 +0700

    update

---
+++ WebChat/WebChat/Controllers/WebChatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebChat.Models;
+
+namespace WebChat.Controllers
+{
+    public class WebChatController : Controller
+    {
+        // GET: WebChat
+        [HttpGet]
+        [CheckAuthorization]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
