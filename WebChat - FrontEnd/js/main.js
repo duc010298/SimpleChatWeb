dd98a236b6ef4bd6cb1bd8e29f57e200f2c7cf9c
NguyenDucThien98 <39368428+NguyenDucThien98@users.noreply.github.com>
2019-03-06 00:29:11 +0700

    update form

--- WebChat - FrontEnd/js/main.js
+++ WebChat - FrontEnd/js/main.js
@@ -1,1 +1,12 @@
 // All function and code javascript and jquery here
+function loadRegister() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("login").innerHTML =
+        this.responseText;
+      }
+    };
+    
+    xhttp.send();
+  }
