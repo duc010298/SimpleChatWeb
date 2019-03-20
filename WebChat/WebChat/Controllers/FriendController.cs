fa1d3dd67d7caa7864b025ceb70ce50fe0a3ac63
duc010298 <duc010298@gmail.com>
2019-03-20 10:16:36 +0700

    search friend

--- WebChat/WebChat/Controllers/FriendController.cs
+++ WebChat/WebChat/Controllers/FriendController.cs
@@ -61,6 +61,46 @@ namespace WebChat.Controllers
             return Json(FriendForListFriend);
         }
 
+        [HttpPost]
+        [CheckAuthorization]
+        public ActionResult GetFriendSearch(int page, string input)
+        {
+            int numberFriendInOnePage = 30;
+            Guid userID = (Guid)Session["UserID"];
+            List<FriendForListFriend> FriendForListFriend = new List<FriendForListFriend>();
+            using (var db = new WebChatEntities())
+            {
+                var listRelationShip = db.relationships.Where(s => ((s.cus1_id == userID && s.customer1.fullname.Contains(input)) 
+                || (s.cus2_id == userID && s.customer.fullname.Contains(input))) 
+                && (s.relationship_status == 1 || s.relationship_status == 2)).OrderBy(s => s.relationship_id)
+                    .Skip(numberFriendInOnePage * (page - 1)).Take(numberFriendInOnePage).ToList();
+                foreach (var rela in listRelationShip)
+                {
+                    if (rela.cus1_id.Equals(userID))
+                    {
+                        var friendInfo = db.customers.Where(s => s.app_user_id == rela.cus2_id).FirstOrDefault();
+                        FriendForListFriend temp = new FriendForListFriend();
+                        temp.Id = friendInfo.app_user_id.ToString();
+                        temp.Avatar = friendInfo.avatar;
+                        temp.Fullname = friendInfo.fullname;
+                        temp.RelationshipStatus = rela.relationship_status;
+                        FriendForListFriend.Add(temp);
+                    }
+                    else
+                    {
+                        var friendInfo = db.customers.Where(s => s.app_user_id == rela.cus1_id).FirstOrDefault();
+                        FriendForListFriend temp = new FriendForListFriend();
+                        temp.Id = friendInfo.app_user_id.ToString();
+                        temp.Avatar = friendInfo.avatar;
+                        temp.Fullname = friendInfo.fullname;
+                        temp.RelationshipStatus = rela.relationship_status;
+                        FriendForListFriend.Add(temp);
+                    }
+                }
+            }
+            return Json(FriendForListFriend);
+        }
+
         [HttpGet]
         [CheckAuthorization]
         public ActionResult Unfriend(string id)
